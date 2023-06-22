05811b63b2d362b67e56f3a11c0937779ffb4101
alaminthespecial <alaminthespecial@gmail.com>
2023-06-22 09:17:16 -0400

    fourth commit

--- app/src/main/java/com/techhaal/little_lemon/Home.kt
+++ app/src/main/java/com/techhaal/little_lemon/Home.kt
@@ -82,16 +82,114 @@ fun HomeNavigate(navController: NavHostController) {
                         Image(
                             painter = painterResource(R.drawable.hero_image),
                             contentDescription = "Hero Image",
-                            modifier = Modifier.width(190.dp).height(200.dp),
+                            modifier = Modifier
+                                .width(190.dp)
+                                .height(200.dp),
                             contentScale = ContentScale.Crop
                         )
 
                     }
+                Spacer(modifier = Modifier.height(4.dp))
+                Box(
+
+                    modifier = Modifier.height(20.dp).width(200.dp)
+                        .background(Color(0xFFA5B8B1)
+                            ) ,
+                    contentAlignment = Alignment.Center,
+
+                ){
+                    Row() {
+
+                    }
+                }
+
 
 
             }
         }
+        Text(
+            "ORDER FOR DELIVERY!",
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF0E0B05),
+            style = MaterialTheme.typography.headlineSmall
+        )
+        Row(
 
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+
+                "Starter",
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFF070500),
+                style = MaterialTheme.typography.headlineSmall,
+                        fontSize = 14.sp,
+            )
+            Spacer(modifier = Modifier.width(40.dp))
+            Text(
+                "Mains",
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFF020100),
+                style = MaterialTheme.typography.headlineSmall,
+                fontSize = 14.sp,
+            )
+            Spacer(modifier = Modifier.width(40.dp))
+
+
+            Text(
+                "Dessert",
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFF090600),
+                style = MaterialTheme.typography.headlineSmall,
+                fontSize = 14.sp,
+            )
+            Spacer(modifier = Modifier.width(40.dp))
+
+            Text(
+                "Size",
+                fontWeight = FontWeight.Bold,
+                fontSize = 14.sp,
+                color = Color(0xFF090600),
+                style = MaterialTheme.typography.headlineSmall
+            )
+        }
+        Text(
+            "Greek Salad",
+            fontWeight = FontWeight.Bold,
+
+            color = Color(0xFF090600),
+            style = MaterialTheme.typography.headlineSmall
+        )
+        Column(modifier = Modifier.padding(8.dp)) {
+
+            Row(
+
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+
+                Text(
+                    "The famous greek salad of crispy\n lettese,peper,olive and our Chigao",
+                    color = Color(0xFF090000),
+                    fontSize = 14.sp,
+                    style = MaterialTheme.typography.headlineSmall
+                )
+                Spacer(modifier = Modifier.width(20.dp))
+                Image(
+                    painter = painterResource(R.drawable.hero_image),
+                    contentDescription = "Hero Image",
+                    modifier = Modifier
+                        .width(120.dp)
+                        .height(100.dp),
+                    contentScale = ContentScale.Crop
+                )
+
+            }
+
+
+
+        }
     }
 
 }
